299b4d3418fc918edf980e92aa829d6a74626d52
SabreOfWinter <steadmanbailey@gmail.com>
2021-08-16 17:05:29 +0100

    Create class folders to store sequences

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+import os
+
 def extract_videos():
     videos_train_path = '' #Directory for videos to be classified. Every video should be divided into folders for each class (E.g. './Data/Sign_hello/' would have all the videos of the sign hello) 
     frames_train_path = '' #Directory for extracted sequences of frames to be placed
+
+    #Check for if frame folders have been created and create them if not
+    for label in os.listdir(videos_train_path):
+        if label not in os.listdir(frames_train_path):
+            os.mkdir(frames_train_path + '/' + label)
 
 def main():
     pass
