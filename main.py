3aa269885a718ad238673d1971e6d85878118ce6
SabreOfWinter <steadmanbailey@gmail.com>
2021-08-17 09:43:24 +0100

    Convert SavedModel to TFLite

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import tensorflow as tf
 
 def extract_videos():
     videos_train_path = '' #Directory for videos to be classified. Every video should be divided into folders for each class (E.g. './Data/Sign_hello/' would have all the videos of the sign hello) 
@@ -18,6 +19,18 @@ def extract_videos():
         for video in os.listdir(videos_path):
             video_path = videos_path + '/' + video
             video_name = video.split('.')[0]
+
+##########
+    #Used to convert LRCN SavedModel format to .tflite format
+##########
+def convert_model():
+    saved_model_dir = '' #path to SavedModel format of selected model to convert
+    converter = tf.lite.TFLiteConverter.from_saved_model(saved_model_dir)
+    tflite_model = converter.convert()
+
+    # Save the model
+    with open('model.tflite', 'wb') as f:
+        f.write(tflite_model)
 
 def main():
     pass
